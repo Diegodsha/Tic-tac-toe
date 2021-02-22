9bce7dae8c5d626b70b6a15afd8e3ae0a4f17bd8
Rashad-Muntar <muntartouregmail.com>
2021-02-22 07:29:37 -0800

    add tic tac toe board

---
+++ lib/board.rb
@@ -0,0 +1,13 @@
+class Board
+    def initialize
+        @board = Array.new(9, " ")
+    end
+
+    def show_board
+        puts "#{@board[0] = 1} | #{@board[1] = 2} | #{@board[2] = 3}"
+        puts "-----------"
+        puts "#{@board[3] = 4} | #{@board[4] = 5} | #{@board[5] = 6}"
+        puts "-----------"
+        puts "#{@board[6] = 7} | #{@board[7] = 8} | #{@board[8] = 9}"
+    end
+end
