c43c39364d5bbbbc04341a26cdb9bc93d421ed92
Diego <diegodsha@hotmail.com>
2021-02-23 14:10:03 -0600

    Remove uneccessary files and comments

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,4 +1,3 @@
-# require_relative '../bin/main.rb'
 class TicTacToe
   WIN_COMBINATION = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [6, 4, 2], [0, 4, 8]].freeze
   attr_reader :current_player, :current_mark, :mark_one, :mark_two, :game_finish
