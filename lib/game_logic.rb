150a9d846f712a9b97ad7df5446b2687afa8e649
Diego <diegodsha@hotmail.com>
2021-02-23 15:10:10 -0600

    Add tips before players move

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -14,13 +14,12 @@ class TicTacToe
   end
 
   def show_board
-   %(
+    %(
       #{@board[0]} | #{@board[1]} | #{@board[2]}
       ----------
       #{@board[3]} | #{@board[4]} | #{@board[5]}
       ----------
       #{@board[6]} | #{@board[7]} | #{@board[8]})
-
   end
 
   def win_check?
