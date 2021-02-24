717d150a728966a905cc408069cf431ab10c4e76
Rashad-Muntar <muntartouregmail.com>
2021-02-24 12:10:58 -0800

    resolve rubocop errors

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,4 +1,4 @@
-require_relative "../lib/player"
+require_relative '../lib/player'
 
 class TicTacToe
   WIN_COMBINATION = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [6, 4, 2], [0, 4, 8]].freeze
--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,6 @@
 class Player
   attr_reader :player, :mark
-  
+
   def initialize(player, mark)
     @player = player
     @mark = mark
